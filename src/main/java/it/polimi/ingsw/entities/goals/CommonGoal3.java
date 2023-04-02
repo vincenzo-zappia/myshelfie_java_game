5961b01cc626350d65d6d4af08973916727691aa
Vinello28 <74827522+Vinello28@users.noreply.github.com>
2023-04-02 20:13:23 +0200

    CommonGoal1, domani tutti i 12 e batteria di test

---
+++ src/main/java/it/polimi/ingsw/entities/goals/CommonGoal3.java
@@ -0,0 +1,18 @@
+package it.polimi.ingsw.entities.goals;
+
+import it.polimi.ingsw.entities.Bookshelf;
+import it.polimi.ingsw.entities.Cell;
+
+public class CommonGoal3 implements Goal{
+    @Override
+    public int checkGoal(Bookshelf bookshelf) {
+        int tmp=0;
+        int points=0;
+
+        for(int i = 0; i < 5; i++){
+            //TODO finire implementando metodo di vince
+        }
+
+        return 0;
+    }
+}
