49be925aebb07a14fc4fe31f8590d8fab153b2d3
Vinello28 <74827522+Vinello28@users.noreply.github.com>
2023-04-03 15:17:16 +0200

    Aggiunto controllo celle già usate

--- src/main/java/it/polimi/ingsw/server/model/entities/goals/CommonGoal1.java
+++ src/main/java/it/polimi/ingsw/server/model/entities/goals/CommonGoal1.java
@@ -7,6 +7,7 @@ import it.polimi.ingsw.server.model.entities.Cell;
 
 public class CommonGoal1 implements Goal{
     private static final int SCORE = 1; //TODO: inserire valore del goal
+    private static final int UNAVAILABLE= 104; //DEBUG use
     private int[][] x = new int[6][5];
 
     //TODO add this method in bookshelf class, and make checkGoal accept an int[][] like parameter.
@@ -32,14 +33,12 @@ public class CommonGoal1 implements Goal{
         for(int i = 0; i < 5 ; i++){  //search from row 0 to row-1
             for(int j = 0; j < 4; j++){ //search from column 0 to column-1
                 if(!bs.getCell(i,j).isCellEmpty()){  //check the cell if it is empty or not
-                    if(tipo==0){
-                        if(x[i][j] == x[i][j+1] && x[i][j] == x[i+1][j] && x[i][j] == x[i+1][j+1]){
-                            tmp++;  //when one found increment tmp
-                            tipo=x[i][j]; //used in second if to check that they are of the same type/color
-                        }
-                    }
-                    if(tipo == x[i][j] && x[i][j] == x[i][j+1] && x[i][j] == x[i+1][j] && x[i][j] == x[i+1][j+1]){
+                    if(x[i][j] == x[i][j+1] && x[i][j] == x[i+1][j] && x[i][j] == x[i+1][j+1] && x[i][j] != UNAVAILABLE){
                         tmp++;  //when one found increment tmp
+                        x[i][j] = UNAVAILABLE;
+                        x[i][j+1] = UNAVAILABLE;
+                        x[i+1][j] = UNAVAILABLE;
+                        x[i+1][j+1] = UNAVAILABLE;
                     }
                 }
             }
