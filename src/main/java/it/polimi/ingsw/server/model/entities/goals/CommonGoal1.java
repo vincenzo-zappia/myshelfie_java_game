507b92591385b6e71d8396cb98e2d576cd397753
Vincenzo Zappia <vincenzo.zappia@mail.polimi.it>
2023-04-03 02:23:33 +0200

    Update CommonGoal1.java

--- src/main/java/it/polimi/ingsw/server/model/entities/goals/CommonGoal1.java
+++ src/main/java/it/polimi/ingsw/server/model/entities/goals/CommonGoal1.java
@@ -4,7 +4,7 @@ import it.polimi.ingsw.server.model.entities.Bookshelf;
 import it.polimi.ingsw.exceptions.CellGetCardException;
 
 public class CommonGoal1 implements Goal{
-    private static final int SCORE = 0; //TODO: inserire valore del goal
+    private static final int SCORE = 1; //TODO: inserire valore del goal
     @Override
     public int checkGoal(Bookshelf bs) {
         //equal square card
@@ -12,7 +12,7 @@ public class CommonGoal1 implements Goal{
         try {
             for(int i = 0; i < 5 ; i++){  //search from row 0 to row-1
                 for(int j = 0; j < 4; j++){ //search from column 0 to column-1
-                    if(!bs.getCell(i,j).isCellEmpty()){  //check the cell if it is empty or not
+                    if(!bs.getCell(i,j).isCellEmpty() && !bs.getCell(i,j+1).isCellEmpty() && !bs.getCell(i+1,j).isCellEmpty() && !bs.getCell(i+1,j+1).isCellEmpty()){  //check the cell if it is empty or not
                         if(bs.getCell(i,j).getCard().equals(bs.getCell(i,j+1).getCard())     //if !empty search the 2x2 matrix
                            && bs.getCell(i,j).getCard().equals(bs.getCell(i+1,j).getCard())
                            && bs.getCell(i,j).getCard().equals(bs.getCell(i+1,j+1).getCard())){
