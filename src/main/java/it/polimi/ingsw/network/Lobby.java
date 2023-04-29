b39ad20053a7065f4f091b64a07e6aaaebba4962
Maurizio Tirabassi <maurizio.tirabassi@mail.polimi.it>
2023-04-30 01:40:22 +0200

    Prototipi metodi logica Lobby
    TODO

--- src/main/java/it/polimi/ingsw/network/Lobby.java
+++ src/main/java/it/polimi/ingsw/network/Lobby.java
@@ -2,47 +2,60 @@ package it.polimi.ingsw.network;
 
 import it.polimi.ingsw.mechanics.Game;
 import it.polimi.ingsw.mechanics.GameController;
-import it.polimi.ingsw.network.messages.GameStarted;
+import it.polimi.ingsw.mechanics.VirtualView;
 import it.polimi.ingsw.network.messages.Message;
-import it.polimi.ingsw.network.messages.MessageType;
 
 import java.util.ArrayList;
-//TODO: classe che crea effettivamente la partita: crea istanza di Game, crea i CommonGoal da associare a Game, associa Game a GameController
-//TODO: e lega i Client ai corrispettivi Player e username
+import java.util.HashMap;
+
 public class Lobby {
 
     //TODO: verificare quali attributi servono effettivamente alla lobby
     //region ATTRIBUTES
     private final int lobbyId;
     private final Server server;
-    private ArrayList<ClientHandler> clientsHandler;
-    private ArrayList<String> playerList;
+    private ArrayList<ClientHandler> clientHandlers;
+    private ArrayList<String> playerUsernames;
+    private ArrayList<NetworkPlayer> networkPlayers;
     private boolean inGame;
+    private final GameController gameController;
     //endregion
-
-    private GameController gameController;
 
     //TODO: DA REVISIONARE (quando la lobby viene creata non si conoscono gli username di tutti i player)
     //TODO: verificare quali attributi servono alla creazione della lobby
 
     public Lobby(Server server, String creatorUsername, int lobbyId) {
-        inGame=false;
         this.lobbyId = lobbyId;
         this.server = server;
+
         addPlayer(creatorUsername);
+        inGame = false;
+
     }
 
+    //TODO: Metodo chiamato da clientHandler per aggiungere se stesso alla Lobby
+    public void joinLobby(ClientHandler clientHandler){
+        //TODO: Come e quando sceglie lo username il giocatore?
+        //addPlayer();
+    }
+
+    //TODO: Fondere con joinLobby()
     public void addPlayer(String username){
         //TODO: implementare la verifica che la lobby non sia piena e restituzione messaggio di accettazione o errore
-        playerList.add(username);
+        playerUsernames.add(username);
+
+        //TODO: Creazione hashmap (username, virtualView). Come viene passato il parametro clientHandler al costruttore di VirtualView?
+        //networkPlayers.add(new NetworkPlayer(username, new VirtualView()));
+
+        //TODO: Una volta aggiunto l'ultimo player procedere con la chiamata di startGame()
     }
-    public Game startGame(){
-        assert (playerList.size()>1 && playerList.size()<=4);
 
+    //TODO: Metodo che deve inizializzare Game e GameController (a cui deve essere passata l'hashmap)
+    public void startGame(){
+        assert (playerUsernames.size()>1 && playerUsernames.size()<=4);
         //TODO: implementare invio messaggio di startgame (dopo verifica dei prerequisiti)
-        //gameController.getClientHandler().sendMessage(new GameStarted(gameController.getCurrentPlayer(), MessageType.GAME_STARTED));
 
-        return new Game(playerList);
+        gameController = new GameController(playerUsernames, new Game(playerUsernames), );
     }
 
     public void sendToController(Message msg){
