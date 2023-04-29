b5ddcec2faaccf400d62152f40b2156f39ef151e
Maurizio Tirabassi <maurizio.tirabassi@mail.polimi.it>
2023-04-29 17:14:12 +0200

    Update TurnManager
    TurnManager is now the class that only implements game rules for the management of the turns. It is used by Lobby to check is the player who sends a message is actually the one who has to play the current turn.

--- src/main/java/it/polimi/ingsw/mechanics/TurnManager.java
+++ src/main/java/it/polimi/ingsw/mechanics/TurnManager.java
@@ -2,72 +2,54 @@ package it.polimi.ingsw.mechanics;
 
 import java.util.ArrayList;
 
+//TurnManager disaccoppiato dal gioco: non ha modo di verificare legalità delle mosse
+//è un "iteratore" che scorre se stesso: unico compito quello di dire chi è il currentPlayer
 public class TurnManager {
 
     //region ATTRIBUTES
-    private final Game game; //TODO: Decidere se accesso diretto o indiretto
     private final ArrayList<String> playerUsernames;
     private String currentPlayer;
     private boolean endGame;
-    private turnPhase currentPhase;
+    //private turnPhase currentPhase;
     //endregion
 
-    enum turnPhase {START, SELECTION, INSERTION, END};
+    //enum turnPhase {START, SELECTION, INSERTION, END};
 
     //region CONSTRUCTOR
-    public TurnManager(ArrayList<String> playerUsernames, Game game){
+    public TurnManager(ArrayList<String> playerUsernames){
         this.playerUsernames = playerUsernames;
-        this.game = game; //TODO: rimuovere o meno in base a decisione
-
+        endGame = false;
     }
     //endregion
 
     //region METHODS
 
     /**
-     * method that calls the first startTurn() and sets the initial phase(?)
+     * Method that sets the current player according to the rules of the game. It circularly iterates
+     * through the players. If the endgame phase started it stops at the last username which corresponds
+     * to the player to the right of the sofa.
      */
-    //TODO: (forse gestito da Lobby)
-    public void startGame(){
-        //TODO: Inizializzazione (fase?)
-
-        startTurn();
-    }
-
-    /**
-     * method that sets the current player and starts the endgame
-     */
-    public void nextTurn(){
+    public boolean nextTurn(){
         int currentPlayerIndex = playerUsernames.indexOf(currentPlayer);
 
         //circular iteration of turns
         if(currentPlayerIndex + 1 < playerUsernames.size()) currentPlayerIndex += 1;
         else if(!endGame) currentPlayerIndex = 0;
-            //the player to the right of the sofa is the last player (the player who goes after the first one is the one to his left because the game turns go clockwise)
-        else {
-            //TODO: spostare implementazione logica di gioco: in base alla fase corrente setatta da TurnController
-            //TODO: Lobby chiamerà gli eventi di gioco
-            //findWinner();
-            return;
-        }
+        else return false;
         currentPlayer = playerUsernames.get(currentPlayerIndex);
+        return true;
     }
 
     /**
-     * method that checks the validity of the player's move and calls the game methods
+     * Method that starts the endGame: the iteration of the player is set to stop to the last username which
+     * corresponds to the player to the right of the sofa.
      */
-    //TODO: review required
-    //TODO: dividere ancor di più le fasi quando sarà da implementarsi la GUI (routine di inizio e fine turno)
-    public void startTurn(){
-        //int playerIndex = playerUsernames.indexOf(currentPlayer);
+    public void startEndGame(){
+        endGame = true;
+    }
 
-        //TODO: Check legalità verrà fatto in Lobby(?) e non nello gestore turni
-        //if the bookshelf is full then the endgame begins
-        if(game.isPlayerBookshelfFull(currentPlayer)) endGame = true;
-
-        //If the game isn't ended then the current player changes and the action of the turn is called again recursively
-        nextTurn();
-        startTurn();
+    public String getCurrentPlayer(){
+        return currentPlayer;
     }
 
     //endregion
