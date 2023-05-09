499cc65242d996087790acc51576e6d5611facd1
Vincenzo Zappia <vincenzo.zappia@mail.polimi.it>
2023-05-09 16:06:09 +0200

    housekeeping + add virtual model

---
+++ src/main/java/it/polimi/ingsw/view/VirtualModel.java
@@ -0,0 +1,13 @@
+package it.polimi.ingsw.view;
+
+import it.polimi.ingsw.util.BoardCell;
+import it.polimi.ingsw.util.Cell;
+
+import java.util.Collection;
+import java.util.HashMap;
+
+public class VirtualModel {
+    private BoardCell[][] board;
+    private Cell[][] bookshelf;
+
+}
