78bc198218d0e20e71ccf6fe44d07c6c0bbcf754
Vincenzo Zappia <vincenzo.zappia@mail.polimi.it>
2023-05-20 19:11:11 +0200

    added network config in gui

---
+++ src/main/java/it/polimi/ingsw/view/gui/scenes/NetworkingScene.java
@@ -0,0 +1,15 @@
+package it.polimi.ingsw.view.gui.scenes;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class NetworkingScene extends GenericScene {
+
+    @FXML private TextField ipTextField;
+    @FXML private TextField portTextField;
+
+    @FXML private void onConnectClick() {
+
+    }
+
+}
