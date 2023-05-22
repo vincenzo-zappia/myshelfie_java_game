78b6f0730ce9b9a39ae3f2c0288031479d2e97a7
Vinello28 <74827522+Vinello28@users.noreply.github.com>
2023-05-22 18:57:34 +0200

    CommonGoal0

--- src/main/java/it/polimi/ingsw/entities/goals/CommonGoal0.java
+++ src/main/java/it/polimi/ingsw/entities/goals/CommonGoal0.java
@@ -3,32 +3,91 @@ package it.polimi.ingsw.entities.goals;
 import it.polimi.ingsw.entities.Bookshelf;
 import it.polimi.ingsw.entities.util.CardType;
 
-/*
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.TreeMap;
+
+/**
  * Questo CommonGoal si riferisce ai Goals presente sulla Board
  */
 public class CommonGoal0 implements Goal{
-    //TODO: revisionare (se quallcuno lo tocca vincenzo utilizzera il metodo kill(); )
-    private int[][] mColor = new int[6][5];
-    private final int[] scores;
 
+    //TODO: Vincè controlla se va bene
+    //private int[][] mColor = new int[6][5];
+    //private final int[] scores;
+   //private int partial;
+    /*
     public void setColorMatrix(int[][] matrix){
         mColor = matrix;
     }
-
+    */
     public CommonGoal0() {
-        scores = new int[]{2,3,5,8};
+        //scores = new int[]{0,3,5,8};
     }
 
     @Override
     public int checkGoal(Bookshelf bookshelf) {
+        /*
+            partial = 0;
+            for(CardType type: CardType.values()){...}
 
-        int partial = 0;
-        for(CardType type: CardType.values()){
+            return findLargestAdjacentGroup(mColor);
+        */
 
-        }
-        return findLargestAdjacentGroup(mColor);
+        return searchGroups(bookshelf.getBookshelfColors());
     }
 
+    /**
+     * Methods that search the max player's group of tiles
+     * @param matrix bookshelf colors
+     * @return group's size
+     */
+    private int searchGroups(int[][] matrix) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        boolean[][] visited = new boolean[rows][cols];
+        ArrayList<Integer> num = new ArrayList<>();
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (!visited[i][j]) {
+                    int value = matrix[i][j];
+                    num.add(findAdjacent(matrix, visited, i, j, value));
+                }
+            }
+        }
+        return Collections.max(num); //TODO: contare solo gruppo più grande o restituire punteggio per tutti gruppi presenti?
+    }
+
+    /**
+     * Check the adjacents tiles and count them
+     * @param matrix int[][] of cards's types
+     * @param visited matrix of already visited cards
+     * @param row to check
+     * @param col to check
+     * @param value contained in tile
+     * @return count of adjacent tiles group
+     */
+    private int findAdjacent(int[][] matrix, boolean[][] visited, int row, int col, int value) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+
+        if (row < 0 || row >= rows || col < 0 || col >= cols || visited[row][col] || matrix[row][col] != value) {
+            return 0;
+        }
+
+        visited[row][col] = true;
+
+        int count = 1;
+        count += findAdjacent(matrix, visited, row - 1, col, value); //Up
+        count += findAdjacent(matrix, visited, row + 1, col, value); //Down
+        count += findAdjacent(matrix, visited, row, col - 1, value); //Left
+        count += findAdjacent(matrix, visited, row, col + 1, value); //Right
+
+        return count;
+    }
+
+    //TODO: se precedenti metodi funzionano, rimuovere sottostanti
+    /*
     private static int findLargestAdjacentGroup(int[][] matrix) {
         int largestGroupSize = 0;
 
@@ -63,5 +122,5 @@ public class CommonGoal0 implements Goal{
 
         return size;
     }
-
+*/
 }
