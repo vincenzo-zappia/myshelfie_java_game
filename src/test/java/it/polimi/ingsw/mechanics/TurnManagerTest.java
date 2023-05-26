ab365bf482b85c662ae20798dc5302a7355b69fc
Maurizio Tirabassi <maurizio.tirabassi@mail.polimi.it>
2023-05-26 16:46:20 +0200

    pshsncych
    Co-Authored-By: Vincenzo Zappia <126266631+vincenzo-zappia@users.noreply.github.com>
    Co-Authored-By: Gabriele Vianello <vianello.tech@gmail.com>

---
+++ src/test/java/it/polimi/ingsw/mechanics/TurnManagerTest.java
@@ -0,0 +1,13 @@
+package it.polimi.ingsw.mechanics;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TurnManagerTest {
+
+    //TODO: TODO
+    @Test
+    void nextTurn() {
+    }
+}
