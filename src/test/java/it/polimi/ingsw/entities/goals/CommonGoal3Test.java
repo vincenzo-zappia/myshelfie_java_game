0f3de116838c20e60604bf0924e4bd5212e7b062
Vincenzo Zappia <vincenzo.zappia@mail.polimi.it>
2023-05-27 10:54:33 +0200

    sync

--- src/test/java/it/polimi/ingsw/entities/goals/CommonGoal3Test.java
+++ src/test/java/it/polimi/ingsw/entities/goals/CommonGoal3Test.java
@@ -2,10 +2,7 @@ package it.polimi.ingsw.entities.goals;
 
 import it.polimi.ingsw.entities.Bookshelf;
 import it.polimi.ingsw.entities.Card;
-import it.polimi.ingsw.exceptions.FullColumnException;
 import it.polimi.ingsw.entities.util.CardType;
-import it.polimi.ingsw.network.Client;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
